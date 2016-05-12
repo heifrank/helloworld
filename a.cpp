dfd9f370943cc742e28047d6eba0045213c6f37a
songyang <songyang@yidian-inc.com>
2016-05-12 19:59:34 +0800

    add a line in a.cpp, modify c.cpp

--- a.cpp
+++ a.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 char ch = 's';
 int x = 99;
+// add a line in a.cpp
 
 int main(){
 	int y = 35;
--- c.cpp
+++ c.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// add a line in c.cpp
+// add a second line in c.cpp
 
 void cc(){
 	std::cout << "in cc" << std::endl;
